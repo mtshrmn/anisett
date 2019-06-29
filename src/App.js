b36d9d47a7d468046344a2c62175bd5c576b6b26
suerflowz <moshesher1998@gmail.com>
2019-06-29 19:38:21 +0300

    add pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from "react";
 import style from "./app.scss";
+import useNavigation from "./stores/navigation";
+
 import {Menu, ShowCard} from "./components";
+import {DownloadsPage} from "./components/Pages";
 
-const App = () => (
-	<div>
-		<Menu />
-		<div className={style.container}>
-			<ShowCard
-				title="Dr. STONE"
-				image="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx105333-pAyqB5RxQlsI.png"
-				progress={5} />
-			<ShowCard
-				title="Given"
-				image="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx108430-LcVOT7ntUbjT.jpg"
-				progress={2} />
+const App = () => {
+	const {active} = useNavigation();
+	return (
+		<div>
+			<Menu />
+			<div className={style.container}>
+				{active === 1 &&
+				<React.Fragment>
+					<ShowCard
+						title="Dr. STONE"
+						image="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx105333-pAyqB5RxQlsI.png"
+						progress={5} />
+					<ShowCard
+						title="Given"
+						image="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx108430-LcVOT7ntUbjT.jpg"
+						progress={2} />
+				</React.Fragment>
+				}
+				{active === 2 &&
+					<DownloadsPage />
+				}
+			</div>
 		</div>
-	</div>
 
-);
+	);
+};
 
 export default App;
