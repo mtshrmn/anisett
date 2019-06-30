717a4f5b0f79d51b858242aeb4ed593664572cb7
suerflowz <moshesher1998@gmail.com>
2019-06-30 16:40:25 +0300

    add precentage next to progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,12 +3,17 @@ import PropTypes from "prop-types";
 import style from "./ProgressBar.module.css";
 
 export const ProgressBar = ({progress}) => (
-	<div className={style.bar}>
-		<div
-			className={style.progress}
-			style={{
-				width: `${progress}%`,
-			}}/>
+	<div className={style.container}>
+		<div className={style.bar}>
+			<div
+				className={style.progress}
+				style={{
+					width: `${progress}%`,
+				}}/>
+		</div>
+		<h5>
+			{`${progress}%`}
+		</h5>
 	</div>
 );
 
