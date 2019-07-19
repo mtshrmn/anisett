5457a7083154997b94299921bc2efa0f5f48584d
suerflowz <moshesher1998@gmail.com>
2019-07-19 12:02:18 +0300

    change downloads to look realistic

--- src/components/Pages/Downloads.js
+++ src/components/Pages/Downloads.js
@@ -5,16 +5,29 @@ export const DownloadsPage = () => (
 	<div>
 		<ShowDownload
 			show={{
-				title: "Lorem Ipsum",
+				title: "Dr. STONE",
 				thumbnail: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx105333-pAyqB5RxQlsI.png",
-				episode: 5,
+				episode: 1,
 			}}
-			downloadProgress={8}
-			upSpeed="420kbps"
-			downSpeed="69Mbps"
+			downloadProgress={100}
+			upSpeed="105kbps"
+			downSpeed="0kbps"
 			peers={300}
 			seeders={80}
-			timeRemaining="40 Minutes"
+			timeRemaining="N/A"
+		/>
+		<ShowDownload
+			show={{
+				title: "Dr. STONE",
+				thumbnail: "https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx105333-pAyqB5RxQlsI.png",
+				episode: 2,
+			}}
+			downloadProgress={38}
+			upSpeed="98kbps"
+			downSpeed="1.1Mbps"
+			peers={300}
+			seeders={80}
+			timeRemaining="12 Minutes"
 		/>
 	</div>
 );
